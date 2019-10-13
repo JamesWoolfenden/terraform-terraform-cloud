0240525e3d6120d5eb1bc1ce6765bb9c4b3fc779
James Woolfenden <james.woolfenden@gmail.com>
2019-10-13 17:07:32 +0100

    add a workspace

--- tfe_workspace.workspace.tf
+++ tfe_workspace.workspace.tf
@@ -6,11 +6,11 @@ resource "tfe_workspace" "workspace" {
   file_triggers_enabled = var.workspace["file_triggers_enabled"]
   queue_all_runs        = var.workspace["queue_all_runs"]
   terraform_version     = var.workspace["terraform_version"]
-  vcs_repo {
+  /*vcs_repo {
     identifier         = var.workspace["identifier"]
     ingress_submodules = false
     oauth_token_id     = data.tfe_workspace.master.vcs_repo.0.oauth_token_id
-  }
+  }*/
 }
 
 data "tfe_workspace" "master" {
